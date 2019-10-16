c4008076af5b373781a13d299a2c2ee42f77ecb3
Philzace1 <unknownumeh@gmail.com>
2019-10-16 22:58:11 +0100

    I hope this thing works oo o

--- server.js
+++ server.js
@@ -20,10 +20,17 @@ app.use('/api/items', items)
 // Serve Static Assests ..if we are in Production
 if(process.env.NODE_ENV === 'production'){
   //set a static folder
-  app.use(express.static('client/build'));
-  app.get('*', (req, res)=>{
-    res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'))
-  })
+  app.use(express.static(path.join(__dirname, "client", "build")))
+
+  
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+
+  // app.get('*', (req, res)=>{
+  //   res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'))
+  // })
 }
 
 const port = process.env.PORT || 5000;
